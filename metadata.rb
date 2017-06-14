26581df05bfb8a8e7470b8e71823ae50805022c0
Yurii Pyvovarov <yuriy.pyvovarov@gmail.com>
2017-06-14 09:09:40 +0200

    Bump version up to 0.8.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'yuriy.pyvovarov@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures Tracks 2.3.0'
 long_description 'Installs/Configures Tracks version 2.3.0. http://www.getontracks.org'
-version '0.1.0'
+version '0.8.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/yupvr/tracks/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/yupvr/tracks' if respond_to?(:source_url)
